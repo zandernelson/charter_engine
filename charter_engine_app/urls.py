1278802fa18df36d0c6ff0dd24ea239031da0883
zandernelson <iskander93@gmail.com>
2024-03-12 00:19:32 -0400

    add booking page

--- charter_engine_app/urls.py
+++ charter_engine_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, booking
 
 urlpatterns = [
     path('', home, name='home'),
+    path('booking/', booking, name='booking'),
 ]
--- charter_engine_app/views.py
+++ charter_engine_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'charter_engine_app/home.html')
+
+def booking(request):
+    return render(request, 'charter_engine_app/booking.html')
