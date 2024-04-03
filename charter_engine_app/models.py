33c3fc3b0a796d1edf624ee129176af0b84350ac
zandernelson <iskander93@gmail.com>
2024-04-02 22:14:56 -0400

    start booking model

--- charter_engine_app/models.py
+++ charter_engine_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+# MODELS
+
+# BOOKING MODEL
+
+class Booking(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    creation_time = models.DateTimeField(auto_now_add=True)
+    confirmation_number = models.CharField(max_length=36, default=uuid.uuid4, unique=True)
+
+    
--- charter_engine_app/tests.py
+++ charter_engine_app/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Booking
+from django.utils import timezone
 
-# Create your tests here.
+# TESTS
+
+# MODEL TESTS
+
+# BOOKING TESTS
+
+class BookingModelTestCase(TestCase):
+    def setUp(self):
+        print("BookingModelTestCase...")
+        self.booking = Booking.objects.create(
+            start_time = timezone.now(),
+            end_time = timezone.now() + timezone.timedelta(hours=2)
+        )
+
+    def test_booking_model(self):
+        self.assertTrue(isinstance(self.booking, Booking))
+        self.assertIsNotNone(self.booking.start_time)
+        self.assertIsNotNone(self.booking.end_time)
+        self.assertIsNotNone(self.booking.creation_time)
+        self.assertIsNotNone(self.booking.confirmation_number)
